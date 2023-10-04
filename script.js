8b2875cdbbfc762f38b9c41d1d1fd90846c0cfb4
Luis Souza <luis.souza@loovi.com.br>
2023-10-04 17:30:55 -0300

    Reduzindo a uma funcao generica recebendo como parametro a cor do semaforo

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const pAmarelo = document.querySelector('.pAmarelo');
 const sinalVerde = document.querySelector('.sinalVerde');
 const pVerde = document.querySelector('.pVerde');
 
-function vermelho(duration, tempoInicio){
+function semaforo(duration, tempoInicio, cor){
     var timer = duration, seconds;
+    if(cor === 'vm'){
+        sinalVermelho.classList.remove('hide');
+        sinalAmarelo.classList.add('hide');
+        sinalVerde.classList.add('hide');
+    }else if(cor === 'a'){
+        sinalVermelho.classList.add('hide');
+        sinalAmarelo.classList.remove('hide');
+        sinalVerde.classList.add('hide');
+    }else if(cor === 'vd'){
+        sinalVermelho.classList.add('hide');
+        sinalAmarelo.classList.add('hide');
+        sinalVerde.classList.remove('hide');
+    }
 
     setInterval(() => {
         seconds = parseInt(timer % tempoInicio, 10);
@@ -16,60 +29,18 @@ function vermelho(duration, tempoInicio){
             seconds = seconds;
         }
     
-        pVermelho.innerHTML = seconds;
+        if(cor === 'vm'){
+            pVermelho.innerHTML = seconds;
+        }else if(cor === 'a'){
+            pAmarelo.innerHTML = seconds;
+        }else if(cor === 'vd'){
+            pVerde.innerHTML = seconds;
+        }
     
         if(--timer < 0){
             timer = 60;
         }
-    }, 400);
+    }, 100);
 }
 
-function amarelo(duration, tempoInicio){
-    var timer = duration, seconds;
-
-    sinalVermelho.classList.add('hide');
-    sinalAmarelo.classList.remove('hide');
-
-    setInterval(() => {
-        seconds = parseInt(timer % tempoInicio, 10);
-        if(seconds < 10){
-            seconds = '0' + seconds;
-        }else{
-            seconds = seconds;
-        }
-    
-        pAmarelo.innerHTML = seconds;
-    
-        if(--timer < 0){
-            timer = 60;
-        }
-    }, 1000);
-}
-
-function verde(duration, tempoInicio){
-
-    var timer = duration, seconds;
-
-    sinalVermelho.classList.add('hide');
-    sinalAmarelo.classList.add('hide');
-    sinalVerde.classList.remove('hide');
-
-    setInterval(() => {
-        seconds = parseInt(timer % tempoInicio, 10);
-        if(seconds < 10){
-            seconds = '0' + seconds;
-        }else{
-            seconds = seconds;
-        }
-    
-        pVerde.innerHTML = seconds;
-    
-        if(--timer < 0){
-            timer = 60;
-        }
-    }, 1000);
-}
-
-// vermelho(60, 60);
-// amarelo(60, 25);
-// verde(60, 40);
+// semaforo(60, 60, 'vd');
