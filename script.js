f98659021be3b315a9f932f05cec13453a872705
Luis Souza <luis.souza@loovi.com.br>
2023-10-05 14:01:44 -0300

    Estilizando

--- script.js
+++ script.js
@@ -5,48 +5,48 @@ const pAmarelo = document.querySelector('.pAmarelo');
 const sinalVerde = document.querySelector('.sinalVerde');
 const pVerde = document.querySelector('.pVerde');
 
-function semaforo(duration, tempoInicio){
-    var contador = 0;
-    var timer = duration, seconds;
+// function semaforo(duration, tempoInicio){
+//     var contador = 0;
+//     var timer = duration, seconds;
 
-    setInterval(() => {
-        seconds = parseInt(timer % tempoInicio, 10);
-        if(seconds < 10){
-            seconds = '0' + seconds;
-        }else{
-            seconds = seconds;
-        }
+//     setInterval(() => {
+//         seconds = parseInt(timer % tempoInicio, 10);
+//         if(seconds < 10){
+//             seconds = '0' + seconds;
+//         }else{
+//             seconds = seconds;
+//         }
         
-        contador++;
-        console.log(contador);
-        if(contador < 60){
-            sinalVermelho.classList.remove('hide');
-            sinalAmarelo.classList.add('hide');
-            sinalVerde.classList.add('hide');
-            pVermelho.innerHTML = seconds;
-        }else if(contador < 70){
-            sinalVermelho.classList.add('hide');
-            sinalAmarelo.classList.remove('hide');
-            sinalVerde.classList.add('hide');
-            pAmarelo.innerHTML = seconds;
-        }else if(contador < 95){
-            sinalVermelho.classList.add('hide');
-            sinalAmarelo.classList.add('hide');
-            sinalVerde.classList.remove('hide');
-            pVerde.innerHTML = seconds;
-        }
+//         contador++;
+//         console.log(contador);
+//         if(contador < 60){
+//             sinalVermelho.classList.remove('hide');
+//             sinalAmarelo.classList.add('hide');
+//             sinalVerde.classList.add('hide');
+//             pVermelho.innerHTML = seconds;
+//         }else if(contador < 70){
+//             sinalVermelho.classList.add('hide');
+//             sinalAmarelo.classList.remove('hide');
+//             sinalVerde.classList.add('hide');
+//             pAmarelo.innerHTML = seconds;
+//         }else if(contador < 95){
+//             sinalVermelho.classList.add('hide');
+//             sinalAmarelo.classList.add('hide');
+//             sinalVerde.classList.remove('hide');
+//             pVerde.innerHTML = seconds;
+//         }
     
-        if(--timer < 0){
-            timer = 60;
-        }
-    }, 100);
-}
+//         if(--timer < 0){
+//             timer = 60;
+//         }
+//     }, 100);
+// }
 
 // semaforo(60, 60);
 
 function vermelho(){
     const duration = 60;
-    const tempoInicio = 60;
+    const tempoInicio = 20;
     var contador = -1;
     var timer = duration, seconds;
 
@@ -146,6 +146,6 @@ function verde(){
     }, 1000);
 }
 
-vermelho();
-setTimeout(amarelo, 60000);
-setTimeout(verde, 70000);
+// vermelho();
+// setTimeout(amarelo, 20000);
+// setTimeout(verde, 30000);
