5ab5b0525852ceef3980de06636b6051735a57df
Luis Souza <luis.souza@loovi.com.br>
2023-10-05 13:17:15 -0300

    Melhorando logica

--- script.js
+++ script.js
@@ -43,3 +43,109 @@ function semaforo(duration, tempoInicio){
 }
 
 // semaforo(60, 60);
+
+function vermelho(){
+    const duration = 60;
+    const tempoInicio = 60;
+    var contador = -1;
+    var timer = duration, seconds;
+
+    const intervalVermelho = setInterval(() => {
+        seconds = parseInt(timer % tempoInicio, 10);
+        if(seconds < 10){
+            seconds = '0' + seconds;
+        }else{
+            seconds = seconds;
+        }
+        
+        contador++;
+        console.log(contador);
+
+        sinalVermelho.classList.remove('hide');
+        pVermelho.innerHTML = seconds;
+
+        if(contador === 60){
+            clearInterval(intervalVermelho);
+            console.log(`CONTADOR: ${contador}`);
+            contador = 0;
+            console.log(`CONTADOR: ${contador}`);            
+        }
+    
+        if(--timer < 0){
+            timer = 60;
+        }
+    }, 1000);
+}
+
+function amarelo(){
+    const duration = 60;
+    const tempoInicio = 10;
+    var contador = -1;
+    var timer = duration, seconds;
+
+    const intervalVermelho = setInterval(() => {
+        seconds = parseInt(timer % tempoInicio, 10);
+        if(seconds < 10){
+            seconds = '0' + seconds;
+        }else{
+            seconds = seconds;
+        }
+        
+        contador++;
+        console.log(contador);
+
+        sinalVermelho.classList.add('hide');
+        sinalAmarelo.classList.remove('hide');
+        pAmarelo.innerHTML = seconds;
+
+        if(contador === 10){
+            clearInterval(intervalVermelho);
+            console.log(`CONTADOR: ${contador}`);
+            contador = 0;
+            console.log(`CONTADOR: ${contador}`);            
+        }
+    
+        if(--timer < 0){
+            timer = 60;
+        }
+    }, 1000);
+}
+
+function verde(){
+    const duration = 60;
+    const tempoInicio = 30;
+    var contador = -1;
+    var timer = duration, seconds;
+
+    const intervalVermelho = setInterval(() => {
+        seconds = parseInt(timer % tempoInicio, 10);
+        if(seconds < 10){
+            seconds = '0' + seconds;
+        }else{
+            seconds = seconds;
+        }
+        
+        contador++;
+        console.log(contador);
+
+        sinalVermelho.classList.add('hide');
+        sinalAmarelo.classList.add('hide');
+        sinalVerde.classList.remove('hide');
+        pVerde.innerHTML = seconds;
+
+        if(contador === tempoInicio){
+            clearInterval(intervalVermelho);
+            console.log(`CONTADOR: ${contador}`);
+            contador = 0;
+            console.log(`CONTADOR: ${contador}`);            
+        }
+    
+        if(--timer < 0){
+            timer = 60;
+        }
+    }, 1000);
+}
+
+vermelho();
+setTimeout(amarelo, 60000);
+setTimeout(verde, 70000);
